f51d2356df08ac76cc3b8d406a4c66002325dc86
CRBroughton <crbroughton@posteo.uk>
2024-04-10 20:32:06 +0100

    move login

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,9 @@
 <template>
   <div >
-    <h3 class="font-bold text-xl mb-10">
-      Nuxt 3 Modules for Domain Driven Design
-    </h3>
-    <nuxt-link class="hover:underline" :to="{ name: 'blog' }"
-      >Open Blog Module</nuxt-link
-    >
-    <nuxt-link class="hover:underline" :to="{ name: 'blog-2' }"
-      >Open Blog Module</nuxt-link
-    >
+    <Login />
   </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
